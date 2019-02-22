cae3a45e624513f73584e86966429f83974a152e
winsagetech <winsagetech@outlook.com>
2019-02-21 21:39:39 -0500

    Remove the real key

--- application/modules/template/views/googleanalytics.php
+++ application/modules/template/views/googleanalytics.php
@@ -2,5 +2,5 @@
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-129277472-2', 'auto');
+  ga('create', 'XXXXXXXXX', 'auto');
   ga('send', 'pageview');</script>
