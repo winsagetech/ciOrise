4e84b5072d593e678339ca646c5f2f9fbc152bb0
Khushwant Sahay <KhushwantSahay@users.noreply.github.com>
2019-03-01 21:16:57 +0530

    Enable link of tnc & Privacy

--- application/modules/template/views/footer.php
+++ application/modules/template/views/footer.php
@@ -64,7 +64,7 @@ function googleTranslateElementInit() {
                 <div class="footer-social">
                     <div class="row">
                         <div class="col-sm-6">
-                            <a href="<?php echo base_url();  ?>tnc">Privacy policy</a>  &nbsp; | &nbsp; <a href="https://idee.winsagetech.com" target="_blank"></i> Powered by Winsage Technologies</a> 
+                            <a href="<?php echo base_url();  ?>privacy">Privacy policy</a>  &nbsp; |&nbsp; <a href="<?php echo base_url();  ?>tnc">Terms and Conditions</a>  &nbsp; | &nbsp; <a href="https://idee.winsagetech.com" target="_blank"></i> Powered by Winsage Technologies</a> 
                         </div>
                         <div class="col-sm-6 links">
                             <ul>
